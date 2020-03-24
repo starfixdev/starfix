7567ec2c3566db73dfc75629b26f5836fca1f9dc
Fahad Israr <fahad00cms@gmail.com>
2020-03-25 03:34:01 +0530

    URL handler for Linux developed with  Quarkus

---
+++ url_handlers_using_quarkus/linux/src/main/java/org/acme/getting/started/linux_url_handler.java
@@ -0,0 +1,30 @@
+package org.acme.getting.started;
+import java.io.*;
+/*Adding Custom URL Handler to Linux for ide:// */
+ public class linux_url_handler{
+     public static void main(String[] args) throws Exception{
+
+             Runtime run = Runtime.getRuntime();  
+            //The best possible I found is to construct a command which you want to execute  
+            //as a string and use that in exec. If the batch file takes command line arguments  
+            //the command can be constructed a array of strings and pass the array as input to  
+            //the exec method. The command can also be passed externally as input to the method.  
+
+            Process p = null;  
+            String cmd = "sudo bash install";  
+            try {  
+                p = run.exec(cmd);  
+
+                p.getErrorStream();  
+                p.waitFor();
+
+            }  
+            catch (IOException e) {  
+                e.printStackTrace();  
+                System.out.println("ERROR.RUNNING.CMD");  
+
+            }finally{
+                p.destroy();
+            }  
+     }
+ }
