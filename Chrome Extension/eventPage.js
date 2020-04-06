d77dfc7885a91d8b1577636d09eafc8df63c5d05
AtharvaThorve <aaathorve@gmail.com>
2020-04-06 13:24:48 +0530

    Basic setup added

---
+++ Chrome Extension/eventPage.js
@@ -0,0 +1,7 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	if (request.todo === 'showPageAction') {
+		chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+			chrome.pageAction.show(tabs[0].id);
+		});
+	}
+});
