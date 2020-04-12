bad29c55f9004262564c323e2f1e467cc2fe1e7c
Fahad Israr <fahad00cms@gmail.com>
2020-04-12 15:27:12 +0530

    Code structure improved

--- chrome_extension_starfish_ide_from_scratch/content.js
+++ chrome_extension_starfish_ide_from_scratch/content.js
@@ -1,52 +1,51 @@
+current_url = window.location.href;
+var page_uri = "ide://#" + window.location.href;
 
-current_url=window.location.href;
-var page_uri ="ide://#"+window.location.href;
- 
-if(current_url.indexOf("https://github.com/")!=-1)
+if (current_url.indexOf("https://github.com/") != -1)
 
-{//Code for Github Begins
+{ //Code for Github Begins
 
-  if(!localStorage.getItem("starfish")){
-     navigator.registerProtocolHandler("web+ide",
-                           "https://github.com/?ide=%s",
-                           "ide");
-     localStorage.setItem("starfish","true");
-  }
-var link = document.createElement('a');
-link.setAttribute("title","Open In IDE");
-//var page_uri ="ide://#"+window.location.href;
-link.setAttribute("href",page_uri);
-link.setAttribute("class","btn btn-sm btn-primary dev")
-link.innerHTML="Open in IDE";
+    if (!localStorage.getItem("starfish")) {
+        navigator.registerProtocolHandler("web+ide",
+            "https://github.com/?ide=%s",
+            "ide");
+        localStorage.setItem("starfish", "true");
+    }
+    var link = document.createElement('a');
+    link.setAttribute("title", "Open In IDE");
+    //var page_uri ="ide://#"+window.location.href;
+    link.setAttribute("href", page_uri);
+    link.setAttribute("class", "btn btn-sm btn-primary dev")
+    link.innerHTML = "Open in IDE";
 
-var starfish=document.createElement('div');
-//starfish.setAttribute("class","empty-icon position-relative gitpod-nav-btn float-right");
-starfish.appendChild(link);
-var el=document.getElementsByClassName("file-navigation")[0];
-el.appendChild(starfish); 
+    var starfish = document.createElement('div');
+    //starfish.setAttribute("class","empty-icon position-relative gitpod-nav-btn float-right");
+    starfish.appendChild(link);
+    var el = document.getElementsByClassName("file-navigation")[0];
+    el.appendChild(starfish);
 
 } //Code for Github Ends here
-if(current_url.indexOf("https://gitlab.com")!=-1)
-{//Code for Gitlab begins here
-  if(!localStorage.getItem("starfish")){
-    navigator.registerProtocolHandler("web+ide",
-                          "https://gitlab.com/?ide=%s",
-                          "ide");
-    localStorage.setItem("starfish","true");
- }
-var link = document.createElement('a');
-link.setAttribute("title","Open In IDE");
+else
+if (current_url.indexOf("https://gitlab.com") != -1) { //Code for Gitlab begins here
+    if (!localStorage.getItem("starfish")) {
+        navigator.registerProtocolHandler("web+ide",
+            "https://gitlab.com/?ide=%s",
+            "ide");
+        localStorage.setItem("starfish", "true");
+    }
+    var link = document.createElement('a');
+    link.setAttribute("title", "Open In IDE");
 
-link.setAttribute("href",page_uri);
-link.setAttribute("class","btn btn-primary clone-dropdown-btn qa-clone-dropdown")
-link.innerHTML="Open in IDE";
+    link.setAttribute("href", page_uri);
+    link.setAttribute("class", "btn btn-primary clone-dropdown-btn qa-clone-dropdown")
+    link.innerHTML = "Open in IDE";
 
-var starfish=document.createElement('div');
-//starfish.setAttribute("class","empty-icon position-relative gitpod-nav-btn float-right");
-starfish.setAttribute("class","git-clone-holder js-git-clone-holder margin-left-5")
-starfish.appendChild(link);
-var el=document.getElementsByClassName("tree-controls")[0];
-el.appendChild(starfish); 
+    var starfish = document.createElement('div');
+    //starfish.setAttribute("class","empty-icon position-relative gitpod-nav-btn float-right");
+    starfish.setAttribute("class", "git-clone-holder js-git-clone-holder margin-left-5")
+    starfish.appendChild(link);
+    var el = document.getElementsByClassName("tree-controls")[0];
+    el.appendChild(starfish);
 
 
 }
@@ -54,4 +53,3 @@ el.appendChild(starfish);
 
 
 //document.getElementsByClassName("get-repo-select-menu")[0].insertAdjacentHTML("afterend",starfish);
-
