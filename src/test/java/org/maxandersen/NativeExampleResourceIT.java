25207a2d98c7125f212403afb8eed22f59205727
fahad-israr <fahad00cms@gmail.com>
2020-05-08 03:51:20 +0530

    Added project description readme improved @Native-Executable built @Jackson dependency for YAML config @jackson fixed---RegisterForReflection @Linux Installer Added
    Browser Extension Added
    
    Package name modified
    
    .dockerignore file fixed
    
    quarkus-resteasy removed
    
    @Code sharing @Remove unwanted code
    
    @Incase user exists invalid path
    
    Incase user already cloned the repo then will launch the Editor Directly;--Linux installer updated
    
    Instructions for Linux  added
    
    Cleaning Installer and Browser Extension'
    
    shifting to root directory
    
    Github Actions wokflow added
    
    factory path removed
    
    Remove debug from src
    
    Removing Github Actions/Workflow
    
    URL validation

---
+++ src/test/java/org/maxandersen/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package org.maxandersen;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
