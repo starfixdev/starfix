9244b0baa2fd2870606c7022588b77fded20310f
fahad-israr <fahad00cms@gmail.com>
2020-05-09 03:44:51 +0530

    Added Command line config  suuport

--- custom_url_handlers_quarkus/src/main/java/org/fahad/starfish.java
+++ custom_url_handlers_quarkus/src/main/java/org/fahad/starfish.java
@@ -17,7 +17,13 @@ import org.simpleyaml.configuration.file.YamlFile;
 class starfish{
     public static void main(String[]args)throws Exception{
         for(String s:args)
-		System.out.println("\n"+s);//Printing Received Arguements for Debugging
+		System.out.println("\nTarget : "+s);//Printing Received Arguements for Debugging
+
+		if(args[0].equalsIgnoreCase("config")){
+		//Incase user wants to configure starfish
+		editConfig();//Calling function that enables to edit configuration
+		return; //Incase user typed "starfish config" we only want to edit configuration
+		}
 
 		//Calling function for Fetching installed Packages 
 		//fetch_installed_packages(); //This function will print all installed packages
@@ -30,8 +36,7 @@ class starfish{
 		YamlFile yamlFile = new YamlFile(userHome + "/starfish-config.yml"); //Loading YAML
 
 
-		//Reading input from user
-		//java.io.Console cnsl=System.console();
+		
 		BufferedReader reader= new BufferedReader(new InputStreamReader(System.in));
 		
 		
@@ -39,53 +44,16 @@ class starfish{
 		try {
 			if (!yamlFile.exists()) {
 				//Incase no config file exists
-				System.out.println("\n--------This is the first time you're using starfish...You'll need to  configure it------");
-				System.out.println("\n--------New  configuration file will be created at: " + yamlFile.getFilePath() + "\n");
-				yamlFile.createNewFile(true);
-				//----Configuration file doesn't exist first time user opens starfish so we'll ask user to enter configuration-------
-				
-				//--------------------------First we'll input IDE from user------------------------------
-
-				int id=0;
-				while(id!=1||id!=2||id!=3){
-				System.out.println("\n--------Chose the preferred IDE --------\n 1.for vscode \n 2.for eclipse \n 3.for IntelliJ_IDEA ");
-				
-				id=Integer.parseInt(reader.readLine());
-				
-				if(id==1){ide="code";System.out.println("Selected IDE:VsCode");break;}
-				else
-				if(id==2){ide="eclipse";System.out.println("Selected IDE:Eclipse");break;}
-				else
-				if(id==3){ide="idea";System.out.println("Selected IDE:IntelliJ_IDEA");break;}
-				else
-				System.out.println("Invalid Input Try Again");
-				}
-
-				//-----------Now we'll get preferred clone path on local file system from user--------------
-				System.out.println("Enter preferred Clonning path");
-				clone_path=reader.readLine();
-
-				//---------Save the received configurations in /home/user_name/starfish-config.yml
-				yamlFile.set("ide",ide);                 //Storing IDE configuration in YAML file
-				yamlFile.set("clonning_dir",clone_path ); //Storing  Clone Path configuration  in YAML
-				//--------Save the New Config in YML file-------
-				try{
-					yamlFile.save();
-					System.out.println("Configuration updated and saved successfully");
-				}
-				catch(Exception e){
-					e.printStackTrace();
-					System.out.println("Couldn't save the configurations :(");
-					System.out.print("Press any key to continue . . . ");
-					reader.readLine();
-				}
+				System.out.println("\nThis is the first time you're using starfish...You'll need to  configure it");
+				editConfig(); //Calling function that lets user to configure 
 			}
-			else {
-				System.out.println("\nLoading configurations.....\n");
-				yamlFile.load(); // Loads the entire file
-				ide=yamlFile.getString("ide");
-				clone_path=yamlFile.getString("clonning_dir");
-			}
+			
+			System.out.println("\nLoading configurations.....\n");
+			yamlFile.load(); // Loads the entire file
+			ide=yamlFile.getString("ide");
+			clone_path=yamlFile.getString("clonning_dir");
+			if(ide==null||clone_path==null)editConfig(); //Incase of absence of configuration in file Launch Config 
+			
 			
 		}
 		catch (Exception e) {
@@ -115,6 +83,73 @@ class starfish{
     }
 
 
+	//Function to edit configuration and serves for command line starfish config
+	public static void editConfig()throws Exception{
+	System.out.println("\n------Starfish Configuration Editor------");
+	String clone_path="";//Holds path to  destination Where the Repository Must Be Clonned
+		String ide="";  //Holds command for IDE to open upon
+		String userHome = System.getProperty( "user.home" ); //Get User Home Directory: /home/user_name
+
+		YamlFile yamlFile = new YamlFile(userHome + "/starfish-config.yml"); //Loading YAML
+		
+		BufferedReader reader= new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		//Reading Configuration File
+		try {
+			if (!yamlFile.exists()) {
+				//Incase no config file exists
+				System.out.println("\n-No configurations exist for starfish...You'll have to  configure it");
+				System.out.println("\n-New  configuration file will be created at: " + yamlFile.getFilePath() + "\n");
+				yamlFile.createNewFile(true);
+				}
+				
+				
+				//--------------------------First we'll input preferred IDE from user------------------------------
+
+				int id=0;
+				while(id!=1||id!=2||id!=3){
+				System.out.println("\n--------Chose the preferred IDE --------\n 1.for vscode \n 2.for eclipse \n 3.for IntelliJ_IDEA ");
+				
+				id=Integer.parseInt(reader.readLine());
+				
+				if(id==1){ide="code";System.out.println("Selected IDE:VsCode");break;}
+				else
+				if(id==2){ide="eclipse";System.out.println("Selected IDE:Eclipse");break;}
+				else
+				if(id==3){ide="idea";System.out.println("Selected IDE:IntelliJ_IDEA");break;}
+				else
+				System.out.println("--------Invalid Input Try Again--------");
+				}
+
+				//-----------Now we'll get preferred clone path on local file system from user--------------
+				System.out.println("--------Enter preferred Clonning path--------");
+				clone_path=reader.readLine();
+
+				//---------Save the received configurations in /home/user_name/starfish-config.yml
+				yamlFile.set("ide",ide);                 //Storing IDE configuration in YAML file
+				yamlFile.set("clonning_dir",clone_path ); //Storing  Clone Path configuration  in YAML
+				//--------Save the New Config in YML file-------
+				try{
+					yamlFile.save();
+					System.out.println("--------Configuration updated and saved successfully--------");
+				}
+				catch(Exception e){
+					e.printStackTrace();
+					System.out.println("Couldn't save the configurations :(");
+					System.out.print("Press any key to continue . . . ");
+					reader.readLine();
+				}
+			
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			System.out.print("Press any key to continue . . . ");
+			reader.readLine();
+		}
+	
+	}
 
     public static void gitClone(Path directory, String originUrl) throws IOException, InterruptedException {
         //Function for git clonning
