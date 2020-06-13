889653e254442712517843a2c25fcdd63a10d77d
fahad00cms <fahad00cms@gmail.com>
2020-06-14 04:05:58 +0530

    Path Variable Configuration for starfish

--- ide-protocol-handler/windows/install.cmd
+++ ide-protocol-handler/windows/install.cmd
@@ -3,4 +3,5 @@ cd %~dp0
 xcopy /s/i "%~dp0\Starfish" "C:\Users\Public\Starfish"
 echo Please accept the upcoming Prompts to install successfully
 "%~dp0\starfish.reg" 
+setx path "%path%;C:\Users\Public\Starfish"
 PAUSE
