860a4bb543e9dde6d5597f7a18a7aa2e8433985c
Max Rydahl Andersen <manderse@redhat.com>
2021-07-24 08:20:22 +0200

    fix: remove unnecessary Editor class to make it work in jbang again

--- cli/src/main/java/dev/starfix/Starfix.java
+++ cli/src/main/java/dev/starfix/Starfix.java
@@ -28,7 +28,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import static dev.starfix.Editor.launch_editor;
 
 @CommandLine.Command(mixinStandardHelpOptions = true)
 public class Starfix implements Runnable{
@@ -290,12 +289,34 @@ public class Starfix implements Runnable{
 
     }
 
-    public static interface IDE {
+    public static void launch_editor(Path directory, String ide, String path, String filePath) throws IOException, InterruptedException {
 
+        if(filePath.indexOf("#")>0){
+            filePath = filePath.replace("#L","#");
+            // ==== VS CODE ====
+            // code -g file:line
+            if(ide.equals("code")||ide.equals("code.cmd")){
+                filePath = filePath.replace("#",":");
+                runCommand(directory.getParent(), ide,"-g",path,filePath);
+            }
+            // ===== IntelliJ =====
+            // idea64.exe [--line <number>] [--column <number>] <path ...>
+            // idea /home/fahad/MyProjects/testings/jbang --line 3 /home/fahad/MyProjects/testings/jbang/otp.java
+            if(ide.equals("idea")||ide.equals("idea.exe")||ide.equals("idea64.exe")){
+                String lineNumber  = filePath.substring(filePath.lastIndexOf("#")+1);
+                filePath = filePath.substring(0,filePath.lastIndexOf("#"));
+                runCommand(directory.getParent(), ide,path,"--line",lineNumber,filePath);
+            }
+            // === Eclipse =====
+            // eclipse.exe file.txt:22
+            if(ide.equals("eclipse")){
+                filePath = filePath.replace("#",":");
+                runCommand(directory.getParent(), ide,path,filePath);
+            }
+
+        }
+        else{
+            runCommand(directory.getParent(),ide,path,filePath);// Launching the editor now
+        }
     }
-
-    public static class GenericIDE implements IDE {
-
-    }
-
 }
