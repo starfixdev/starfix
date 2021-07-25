044b433c437d7f4dc17f6c8e47b85ea936d17506
fahad-israr <fahad00cms@gmail.com>
2021-07-25 18:24:51 +0530

    separe ide classes

--- cli/src/main/java/dev/starfix/Starfix.java
+++ cli/src/main/java/dev/starfix/Starfix.java
@@ -113,7 +113,9 @@ public class Starfix implements Runnable{
 
             // Launching Editor on the Cloned Directory
             System.out.println("Launching  Editor Now...");
-            launch_editor(directory, ide, directory.toAbsolutePath().toString(),filePath);
+            GenericIDE genericIde =  new GenericIDE(ide) ;
+            genericIde.getIDE().launch_editor(directory, ide, directory.toAbsolutePath().toString(),filePath);
+
         } catch (Exception e) {
             throw new IllegalStateException(e);
         }
@@ -288,37 +290,108 @@ public class Starfix implements Runnable{
 
     }
 
-    public static void launch_editor(Path directory, String ide, String path, String filePath) throws IOException, InterruptedException {
+    public static abstract class IDE{
+        public abstract void launch_editor(Path directory, String ide, String path, String filePath)throws IOException, InterruptedException;  
 
-        if(filePath.indexOf("#")>0){
-            filePath = filePath.replace("#L","#");
-            // ==== VS CODE ====
-            // code -g file:line
-            if(ide.equals("code")||ide.equals("code.cmd")){
+    }
+
+    public static class GenericIDE extends IDE{
+
+        IDE ide;
+
+        GenericIDE(){
+            // Default Constructor
+        }
+
+        GenericIDE(String ide){
+            setIDE(ide);
+        }
+
+        public IDE getIDE(){
+            //Return Current IDE
+            return ide;
+        }
+
+        public IDE getIDE(String ide){
+            // Will return IDE based on String
+            switch(ide){
+                case "code":
+                case "code.cmd":
+                    return new VsCode();
+
+               case "idea":
+               case "idea64.exe":
+                    return new IntelliJIdea();
+
+               case "eclipse":
+               case "eclipse.exe":
+                    return new Eclipse();
+               default:
+                    throw new IllegalArgumentException("IDE not supported");
+            }
+
+        }
+
+        public void setIDE(String ide){
+            this.ide = getIDE(ide);
+        }
+
+        public  void launch_editor(Path directory, String ide, String path, String filePath)throws IOException, InterruptedException{
+            runCommand(directory.getParent(),ide,path,filePath);
+        }
+
+    }
+
+    public static class VsCode extends IDE{
+        
+        public  void launch_editor(Path directory, String ide, String path, String filePath) throws IOException, InterruptedException {
+            if(filePath.indexOf("#")>0){
+                // code -g file:line
+                filePath = filePath.replace("#L","#");
                 filePath = filePath.replace("#",":");
                 runCommand(directory.getParent(), ide,"-g",path,filePath);
             }
-            // ===== IntelliJ =====
-            // idea64.exe [--line <number>] [--column <number>] <path ...>
-            // idea /home/fahad/MyProjects/testings/jbang --line 3 /home/fahad/MyProjects/testings/jbang/otp.java
-            if(ide.equals("idea")||ide.equals("idea.exe")||ide.equals("idea64.exe")){
+            else{
+               new GenericIDE().launch_editor(directory.getParent(),ide,path,filePath);
+            }
+        }
+    }
+
+    public static class IntelliJIdea extends IDE{
+
+        public  void launch_editor(Path directory, String ide, String path, String filePath) throws IOException, InterruptedException {
+            if(filePath.indexOf("#")>0){
+                filePath = filePath.replace("#L","#");
+                // idea64.exe [--line <number>] [--column <number>] <path ...>
+                // idea /home/fahad/MyProjects/testings/jbang --line 3 /home/fahad/MyProjects/testings/jbang/otp.java
                 String lineNumber  = filePath.substring(filePath.lastIndexOf("#")+1);
                 filePath = filePath.substring(0,filePath.lastIndexOf("#"));
                 runCommand(directory.getParent(), ide,path,"--line",lineNumber,filePath);
             }
-            // === Eclipse =====
-            // eclipse.exe file.txt:22
-            if(ide.equals("eclipse")){
-                filePath = filePath.replace("#",":");
-                runCommand(directory.getParent(), ide,path,filePath);
+            else{
+                runCommand(directory.getParent(),ide,path,filePath);
+                new GenericIDE().launch_editor(directory.getParent(),ide,path,filePath);
             }
-
-        }
-        else{
-            runCommand(directory.getParent(),ide,path,filePath);// Launching the editor now
         }
     }
 
+    public static class Eclipse extends IDE{
 
+        public  void launch_editor(Path directory, String ide, String path, String filePath) throws IOException, InterruptedException {
+            if(filePath.indexOf("#")>0){
+                filePath = filePath.replace("#L","#");
+                // eclipse.exe file.txt:22
+                if(ide.equals("eclipse")){
+                    filePath = filePath.replace("#",":");
+                    runCommand(directory.getParent(), ide,path,filePath);
+                }
+
+            }
+            else{
+                runCommand(directory.getParent(),ide,path,filePath);
+                new GenericIDE().launch_editor(directory.getParent(),ide,path,filePath);
+            }
+        }
+    }
 
 }
